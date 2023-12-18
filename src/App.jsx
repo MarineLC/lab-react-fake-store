71d498f84348776791fb1d714b6db5f8ccc39991
Marine Le Cleach <lecleach.m@gmail.com>
2023-12-18 13:54:12 +0100

    iteration 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import ProductListPage from "./pages/ProductListPage";
 import ProductDetailsPage from "./pages/ProductDetailsPage";
 
+
 import { Routes, Route } from "react-router-dom";
 
 
@@ -11,10 +12,11 @@ function App() {
   return (
     <div className="App relative z-20 pt-20">
       <Navbar />
-
+      
       <Routes>
         <Route path="/" element={<ProductListPage />} />
         <Route path="/product/details/:productId" element={<ProductDetailsPage />} />
+
       </Routes>
 
     </div>
--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,17 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import "../App.css";
 
 
 function ProductListPage() {
   // The state variable `products` is currently an empty array [], 
   // but you should use it to store the response from the Fake Store API (the list of products).
   const [products, setProducts] = useState([]);
-
+ /* const url = "https://fakestoreapi.com/products";
+  const fetchData = async url => {
+       try {
+         const response = await fetch(url)
+         console.log(response)
+         
+       } catch (error) {
+         console.log(error)
+       }
+     }
+*/
+     useEffect(() => {
+      fetch("https://fakestoreapi.com/products")
+      .then(response =>  response.json())
+      .then(apiData => setProducts(apiData));
+      
+     }, [])
   // To fetch the list of products, set up an effect with the `useEffect` hook:
 
 
   return (
     <div className="ProductListPage">
       {/* Render list of products here */}
+    {products.map(products => (
+      <div className='content' key={products.id} >
+        <img src={products.image} alt={products.title} />
+      <li>{products.title}</li>
+      <li>{products.category}</li>
+      <li>{products.price}</li>
+      <li>{products.description}</li>
+   
+ 
+    
+      </div>
+
+
+    ))}
+
     </div>
   );
 }
